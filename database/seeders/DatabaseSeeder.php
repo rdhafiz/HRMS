591e81f7e9708d974b0c7a958f384a34f7fc7e48
Ridwanul Hafiz <ridwanul.hafiz@gmail.com>
2025-10-15 23:50:52 +0600

    training and policies

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,9 +13,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            AdminUserSeeder::class,
+            // AdminUserSeeder::class,
             TrainingAndPolicySeeder::class,
             TrainingPolicyCategorySeeder::class,
+            ParentCategoryTrainingPolicySeeder::class,
         ]);
     }
 }
